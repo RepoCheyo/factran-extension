6f0001f02565ae30224bd464c9029880d8e2dc50
Cedric Arteaga <cedricarteaga@MacBook-Air-de-Cedric.local>
2023-10-27 19:10:51 -0600

    endpoint-instance-update

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://34.228.39.163/api/v1',
+  baseURL: 'https://api.factran.tech/api/v1',
 });
 
 export default instance;
